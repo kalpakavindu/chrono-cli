e985b25a249901ae6669e837368a8c7da7456d76
kalpakavindu <kalpadevonline@gmail.com>
2025-09-13 18:46:30 +0530

    Add Colors module for colored console output.

--- src/Sample.cpp
+++ src/Sample.cpp
@@ -2,6 +2,12 @@
 
 #include <iostream>
 
+#include "Colors.hpp"
+
+using namespace ChronoCLI;
+
 void ChronoCLI::printMessage(const std::string& msg) {
-  std::cout << msg << std::endl;
+  std::cout << Colors::yellow << "Stream test: " << msg << Colors::reset << std::endl;
+
+  std::cout << Colors::blue("String test: " + msg) << std::endl;
 }
